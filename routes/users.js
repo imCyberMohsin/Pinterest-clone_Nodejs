1c536b9b8659302ef8434389d76b3927472982ee
imCyberMohsin <mohsinansari7033@gmail.com>
2024-01-26 10:00:34 +0530

    mongo cloud

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 const plm = require('passport-local-mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/PinterestCloneDB')
+// mongoose.connect('mongodb://127.0.0.1:27017/PinterestCloneDB'); // LocalDB
+mongoose.connect(process.env.MONGO_URL);  // CloudDB
 
 const userSchema = mongoose.Schema({
   fullname: String,
